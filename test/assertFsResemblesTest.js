5e1fb32311e522f02fc6a63ed9405ace3c0ba52d
Rohan Orton <rohan.orton@gmail.com>
2016-03-25 05:41:33 +0000

    Add basic tests

---
+++ test/assertFsResemblesTest.js
@@ -0,0 +1,51 @@
+var assert = require('assert');
+var mock = require('mock-fs');
+var resembles = require('..');
+
+describe('assertFsResembles', function () {
+
+    afterEach(function () {
+        mock.restore();
+    });
+
+    describe('Acceptance:', function () {
+        it('passes if directory contains all files/directories in array', function () {
+            mock({ foo: 'hi', bar: 'hello', baz: 'cia', someDir: {} });
+            resembles([ 'foo', 'bar', 'baz', 'someDir' ]);
+        });
+
+        it('passes if file content is same as specified in object', function () {
+            mock({ foo: 'hi', bar: 'hello', baz: 'cia' });
+            resembles({ foo: 'hi', bar: 'hello', baz: 'cia' });
+        });
+
+        it('passes if nested directory content is same as specified in object', function () {
+            mock({ this_is_a_dir: { foo: 'hi', deeper: { bar: 'hello', baz: 'cia' } } });
+            resembles({ this_is_a_dir: { foo: 'hi', deeper: { bar: 'hello', baz: 'cia' } } });
+        });
+
+    });
+
+    describe('Rejection:', function () {
+        it('throws if directory contains files not passed in array', function () {
+            mock({ foo: 'hi', bar: 'hello', baz: 'cia' });
+            assert.throws(function () {
+                resembles([ 'foo', 'bar' ]);
+            }, /AssertionError/);
+        });
+
+        it('throws if directory contains files not passed in object', function () {
+            mock({ foo: 'hi', bar: 'hello', baz: 'cia' });
+            assert.throws(function () {
+                resembles({ foo: 'hi', bar: 'hello' });
+            }, /AssertionError/);
+        });
+
+        it('throws if existing file content is different than specified in object', function () {
+            mock({ foo: 'hi', bar: 'hello', baz: 'cia' });
+            assert.throws(function () {
+                resembles({ foo: 'wrong', bar: 'text', baz: 'blah' });
+            }, /AssertionError/);
+        });
+    });
+});
